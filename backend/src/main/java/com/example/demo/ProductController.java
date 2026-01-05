f7d1a01690b66bba130c148fc1d97217d92e61c6
Goyda67 <goyda.14881161@gmail.com>
2026-01-05 23:00:42 +0300

    Update ProductController.java

--- backend/src/main/java/com/example/demo/ProductController.java
+++ backend/src/main/java/com/example/demo/ProductController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/products")
 @CrossOrigin(origins = "*")
 public class ProductController {
 
